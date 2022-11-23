c40bc3594bb5bb163f5f2ad83c3601b13d459ad3
Elizabeth <mwendemuthusi6@gmail.com>
2022-11-23 18:43:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+
+// part one- function saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun("bathe my dog"));
+
+
+// part two-function mondayWork
+const mondayWork = function (doWhat= "go to the office") {
+    return `This Monday, I will ${doWhat}.`;
+}
+console.log(mondayWork("work from home"));
+
+
+// part three-again function saturdayFun??
+function wrapAdjective(flair= "*") {
+    const innerFunction = function (character = "special",) {
+        return `You are ${flair}${character}${flair}!`;
+    };
+    return innerFunction;
+}
+
+console.log(wrapAdjective("||")("a hard worker"));
+
+
